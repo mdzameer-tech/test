5d0874935eee1e9721fafac7d69b98e85fe960d0
mdzameer-tech <mdzameer8618@gmail.com>
2025-11-11 17:30:19 +0530

    added reverse LL fxn

--- demo.cpp
+++ demo.cpp
@@ -2,3 +2,37 @@
 
 using namespace std;
 
+
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+
+    }
+
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+
+    }
+};
+
+
+Node* reversell(Node* head){
+    if(head== nullptr || head->next == nullptr) return nullptr;
+    
+    Node* prev = nullptr;
+
+    while(head != nullptr){
+        Node* front = head->next;
+        head->next = prev;
+        head= front;
+    }
+    return prev;
+}
